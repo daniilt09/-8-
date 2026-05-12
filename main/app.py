f45446f4b149148a4bc51e91087aea0d3b654e48
daniilt09 <daniiltimukin207@gmail.com>
2026-05-12 22:32:56 +0700

    Update app.py

--- main/app.py
+++ main/app.py
@@ -3,44 +3,37 @@ from flask import Flask, render_template
 from datetime import datetime
 import os
 import shutil
-import webbrowser
-
-download = True  # менять на False, если не хотим загружать данные с табличек
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, 'data', 'data.json')
-
+BACKUP_DIR = os.path.join(BASE_DIR, 'backups')
 
 def create_backup():
-    backup_dir = os.path.join(BASE_DIR, 'backups')
-    if not os.path.exists(backup_dir):
-        os.makedirs(backup_dir)
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    backup_name = os.path.join(backup_dir, f"data_backup_{timestamp}.json")
-    shutil.copy2(DATA_PATH, backup_name)
-
-
-create_backup()
-
+    if os.path.exists(DATA_PATH):
+        if not os.path.exists(BACKUP_DIR):
+            os.makedirs(BACKUP_DIR)
+        
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        backup_name = os.path.join(BACKUP_DIR, f"data_backup_{timestamp}.json")
+        
+        shutil.copy2(DATA_PATH, backup_name)
 
 def load():
-    with open("data/data.json", "r", encoding="utf-8") as a:
-        data = json.load(a)
-    return data
-
+    if os.path.exists(DATA_PATH):
+        with open(DATA_PATH, "r", encoding="utf-8") as a:
+            return json.load(a)
+    return []
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/dict')
 def dictionary():
     words = load()
     return render_template('dict.html', words=words)
-
 
 @app.route('/word/<int:word_id>')
 def word_detail(word_id):
@@ -50,6 +43,6 @@ def word_detail(word_id):
         return render_template('word.html', word=word)
     return "Слово не найдено", 404
 
-
 if __name__ == '__main__':
+    create_backup()
     app.run(debug=True)
